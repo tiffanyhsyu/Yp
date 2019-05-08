6384ea2ae21d48e4f7bccc7beafe79e58b0582c4
thsyu <thsyu@ucsc.edu>
2019-05-08 16:08:15 -0700

    indentation fix

--- galaxy.py
+++ galaxy.py
@@ -72,7 +72,7 @@ def load_AOS2015(galaxyname):
         full_tbl = Table.read(os.getcwd() + dir + gname, format='ascii', delimiter=' ')
     elif galaxyname == "Test":
         #gname = 'test_output_flux_nir'
-	gname = 'test_output_flux'
+        gname = 'test_output_flux'
         T_OIII = 18000
         full_tbl = Table.read(os.getcwd() + '/' + gname, format='ascii', delimiter=' ')
     else:
